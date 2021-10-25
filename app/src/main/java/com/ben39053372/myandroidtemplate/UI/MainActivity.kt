0a241771834e230ac03b720ecc2cb729fdf72abf
Ben Cheng <ben39053372@gmail.com>
2021-10-25 16:03:52 +0800

    feat: add main Activity

---
+++ app/src/main/java/com/ben39053372/myandroidtemplate/UI/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ben39053372.myandroidtemplate.UI
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ben39053372.myandroidtemplate.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
