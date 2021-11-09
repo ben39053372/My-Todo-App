3e2c7244dc3051a1779efd449f31f2f8f6f78485
Ben Cheng <ben39053372@gmail.com>
2021-11-09 17:26:05 +0800

    add toolbar

--- app/src/main/java/com/ben39053372/myandroidtemplate/UI/MainActivity.kt
+++ app/src/main/java/com/ben39053372/myandroidtemplate/UI/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.ben39053372.myandroidtemplate.UI
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -10,18 +12,31 @@ import androidx.navigation.ui.setupWithNavController
 import com.ben39053372.myandroidtemplate.R
 import com.google.android.material.navigation.NavigationView
 
+
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var drawerView: NavigationView
+    private lateinit var navigationView: NavigationView
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // region define component
+        toolbar = findViewById(R.id.my_toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        // endregion
+
+        // setup navigation ui
         setupNavigationUI()
-        setSupportActionBar(findViewById(R.id.my_toolbar))
+
+        // set Toolbar to ActionBar
+        setSupportActionBar(toolbar)
 
     }
 
@@ -33,13 +48,24 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        // drawer view
-        drawerView = findViewById<NavigationView>(R.id.drawer_view)
-        drawerLayout = findViewById(R.id.drawer_layout)
-
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
-        drawerView.setupWithNavController(navController)
+        navigationView.setupWithNavController(navController)
+
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        actionBarDrawerToggle.syncState()
     }
 
 }
